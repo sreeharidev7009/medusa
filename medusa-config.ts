8d08313fdf35281ec72e0ae3bae872b21d883625
sreeharidev7009 <sreehari.7009@woxro.com>
2025-08-05 16:43:06 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,13 +5,7 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    databaseExtra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
     http: {
-      
       storeCors: process.env.STORE_CORS || "*",
       adminCors: process.env.ADMIN_CORS || "*",
       authCors: process.env.AUTH_CORS || "*",
