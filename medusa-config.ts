9f8fed0ca7f452dbbf2b4d6bcec32c96c08e5455
sreeharidev7009 <sreehari.7009@woxro.com>
2025-08-06 09:40:43 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,7 +4,7 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 export default defineConfig({
   projectConfig: {
-    database_url: process.env.DATABASE_URL,
+    databaseUrl: process.env.DATABASE_URL,
     database_type: "postgres",
     database_extra: {
       ssl: {
