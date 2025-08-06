3ee26e5672dd5d23b0764c7e0eea4d4491c33f2c
sreeharidev7009 <sreehari.7009@woxro.com>
2025-08-06 10:55:43 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,11 +6,6 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL, // PostgreSQL URL from Railway
-   databaseExtra: {
-      ssl: {
-        rejectUnauthorized: false, // Required for managed DBs like Railway, Render, etc.
-      },
-    },
     http: {
       storeCors: process.env.STORE_CORS || "*",
       adminCors: process.env.ADMIN_CORS || "*",
